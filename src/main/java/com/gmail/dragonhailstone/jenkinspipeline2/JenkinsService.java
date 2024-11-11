5faa25373c6e2f381274ad169a07eecbbcf1b026
dragonhail <dragonhailstone@gmail.com>
2024-11-11 14:49:06 +0900

    Auto Build

--- src/main/java/com/gmail/dragonhailstone/jenkinspipeline2/JenkinsService.java
+++ src/main/java/com/gmail/dragonhailstone/jenkinspipeline2/JenkinsService.java
@@ -3,7 +3,7 @@ package com.gmail.dragonhailstone.jenkinspipeline2;
 public class JenkinsService {
     public int  hap(int n){
         int result = 0;
-        for(int i=2; i<=n; i++){
+        for(int i=1; i<=n; i++){
             result += i;
         }
         return result;
