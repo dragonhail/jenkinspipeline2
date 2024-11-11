bdd3a8549e76090758b2b3d96ac660bad7694ef5
dragonhail <dragonhailstone@gmail.com>
2024-11-11 10:07:55 +0900

    initial

---
+++ src/main/java/com/gmail/dragonhailstone/jenkinspipeline2/Jenkinspipeline2Application.java
@@ -0,0 +1,13 @@
+package com.gmail.dragonhailstone.jenkinspipeline2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkinspipeline2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkinspipeline2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/dragonhailstone/jenkinspipeline2/Jenkinspipeline2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.dragonhailstone.jenkinspipeline2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkinspipeline2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
